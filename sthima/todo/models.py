4614f5201861ccde00c4c3279cf82a6dcd153715
Bruno Soares <brunomsss@gmail.com>
2017-05-22 11:03:37 -0300

    Created task model

--- sthima/todo/models.py
+++ sthima/todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    """
+    Stores a single task, related to :model:`auth.User`
+    """
+    position = models.IntegerField()
+    description = models.CharField(max_length=80)
+    done = models.BooleanField(default=False)
+    archived = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        """
+        String representation of a task
+        """
+        return self.description
