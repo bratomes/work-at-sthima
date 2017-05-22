314efe2cf83ac31dbf7926c457973b24bca1c5ff
Bruno Soares <brunomsss@gmail.com>
2017-05-22 13:33:12 -0300

    Created class TodoView and get method

--- sthima/todo/views.py
+++ sthima/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class TodoView(View):
+    """
+    Todo list app index view
+    """
+    def get(self, request):
+        return render(request, 'todo/todo.html')
