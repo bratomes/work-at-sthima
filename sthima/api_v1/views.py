18ca18ad6fca93fbbe1539ea6788d4f043654b56
Bruno Soares <brunomsss@gmail.com>
2017-05-22 22:52:43 -0300

    Created TaskDetail model, get_object method and delete method

--- sthima/api_v1/views.py
+++ sthima/api_v1/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from todo.models import Task
 
@@ -14,3 +17,16 @@ class TaskList(APIView):
         tasks = Task.objects.all()
         serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
+
+
+class TaskDetail(APIView):
+    """
+    Update or Delete a task instance
+    """
+    def get_object(self, pk):
+        return get_object_or_404(Task, pk=pk)
+
+    def delete(self, request, pk, format=None):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
