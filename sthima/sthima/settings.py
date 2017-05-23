2018f28928ae9cf7267b108f2bd98ed3c282da6e
Bruno Soares <brunomsss@gmail.com>
2017-05-22 23:00:07 -0300

    Added rest framework permission setting

--- sthima/sthima/settings.py
+++ sthima/sthima/settings.py
@@ -131,3 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
+}
